06cff9b90dd3a4ae18ea08bd70d07f954df416d3
Angie Molina <amolina1795@gmail.com>
2025-07-12 11:32:44 -0500

    Add uuid and zod dependencies; create event schema type

---
+++ src/types/eventType.ts
@@ -0,0 +1,22 @@
+/*
+•	Evento: corresponde a la representación del ingreso o egreso del usuario. Sus campos deben ser los siguientes:
+o	Id, obligatorio y único por cada evento. Se recomienda el uso de la librería uuid.
+o	Nombre, obligatorio y de máximo 20 caracteres.
+o	Descripción, opcional y de máximo 100 caracteres.
+o	Cantidad, obligatorio y debe ser un número entero o decimal positivo.
+o	Fecha, obligatorio y debe ser una fecha válida
+o	Tipo, un String que solo puede ser “egreso” o “ingreso”.
+*/
+
+import { z } from 'zod';
+
+export const eventSchema = z.object({
+    id: z.uuid("Id is mandatory."),
+    name: z.string("Name is mandatory.").max(20, { error: "The maximum number of characters has been exceeded" }),
+    description: z.string().max(100, { error: "The maximum number of characters has been exceeded" }).optional(),
+    quantity: z.number("Quantity is mandatory.").positive("The number must be positive"),
+    date: z.date("Bad date").min(new Date("2025-01-01"), { error: "Too old" }),
+    type: z.enum(["income", "expense"])
+});
+
+export type EventType = z.infer<typeof eventSchema>;
