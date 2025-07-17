0e0b779d88a0acad6c2d2be75d08683808f4d2be
Angie Molina <amolina1795@gmail.com>
2025-07-16 23:05:51 -0500

    feat: update MantineProvider to use dynamic theme from store and set primary color to indigo

--- src/integrations/mantine/root-provider.tsx
+++ src/integrations/mantine/root-provider.tsx
@@ -1,13 +1,25 @@
 import { MantineProvider, createTheme } from '@mantine/core'
+import useAppStore from '@/store';
 
 type ProviderProps = {
     children?: React.ReactNode
 }
 
-const theme = createTheme({
-    primaryColor: 'yellow',
-})
+const themeConfig = createTheme({
+    primaryColor: 'indigo',
+    defaultRadius: 'md',
+});
 
 export function Provider({ children }: ProviderProps) {
-    return <MantineProvider theme={theme}>{children}</MantineProvider>
+    const theme = useAppStore((state) => state.theme);
+
+    console.log('MantineProvider rendered with theme:', theme);
+
+    return (
+        <MantineProvider
+            theme={themeConfig}
+            forceColorScheme={theme}
+        >
+            {children}
+        </MantineProvider>)
 }
