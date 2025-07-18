fc9fa5cafbe67cb38d07f39be4fb1f4344f94f15
Angie Molina <amolina1795@gmail.com>
2025-07-18 15:59:32 -0500

    feat: add query consult for events

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,60 +1,31 @@
 import { Link, createFileRoute, } from '@tanstack/react-router';
-import { v4 as uuidv4 } from 'uuid';
-import type { EventType } from '@/types/eventType';
+import { useQuery } from '@tanstack/react-query';
 import EventItem from '@/components/event/EventItem';
-
+import DataRepo from '@/api/datasource';
 
 export const Route = createFileRoute('/')({
   component: App,
 })
 
-
-const mockEvents: Array<EventType> = [
-  {
-    id: uuidv4(),
-    name: "Salary",
-    description: "Monthly salary deposit",
-    amount: 2500,
-    date: new Date("2025-07-01"),
-    type: "income",
-    attachment: "payslip_july.pdf",
-  },
-  {
-    id: uuidv4(),
-    name: "Groceries",
-    description: "Supermarket shopping",
-    amount: 120.75,
-    date: new Date("2025-07-05"),
-    type: "expense",
-  },
-  {
-    id: uuidv4(),
-    name: "Freelance",
-    description: "Website redesign project",
-    amount: 800,
-    date: new Date("2025-06-20"),
-    type: "income",
-    attachment: "invoice_freelance.pdf",
-  },
-  {
-    id: uuidv4(),
-    name: "Electricity bill",
-    amount: 60.45,
-    date: new Date("2025-07-10"),
-    type: "expense",
-  },
-  {
-    id: uuidv4(),
-    name: "Gift",
-    description: "Birthday gift from parents",
-    amount: 100,
-    date: new Date("2025-07-12"),
-    type: "income",
-  },
-];
-
-
 function App() {
+
+  const { isPending, error, data: events } = useQuery({
+    queryKey: ['events'],
+    queryFn: () => DataRepo.getEvents(),
+    retry: 3,
+    refetchOnWindowFocus: true,
+    refetchIntervalInBackground: false,
+  });
+
+  if (isPending) {
+    return <div className="p-4">Loading...</div>
+  }
+
+  if (error) {
+    return <div className="p-4 text-red-500">Error: {error.message}</div>
+  }
+
+
   return (
     <div className="text-center dark:bg-zinc-800 dark:text-white">
 
@@ -62,7 +33,7 @@ function App() {
         <Link to="/event/$id" params={{ id: 'new' }} className="text-blue-500 hover:underline">Add event</Link>
       </div>
 
-      {mockEvents.map((mockEvent, index) => (
+      {events.map((mockEvent, index) => (
         <EventItem key={index} id={mockEvent.id} name={mockEvent.name} description={mockEvent.description} amount={mockEvent.amount} date={mockEvent.date} type={mockEvent.type}></EventItem>
       ))}
     </div>
