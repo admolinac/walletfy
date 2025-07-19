399e61075262a62b2573bf10e939c9bdda2899c9
Angie Molina <amolina1795@gmail.com>
2025-07-18 21:36:20 -0500

    feat: improve styles in financial summary

--- src/components/event/FinancialSummary.tsx
+++ src/components/event/FinancialSummary.tsx
@@ -5,25 +5,26 @@ import { currencyFormatter } from "@/utils/formatters";
 export default function FinancialSummary({ events }: { events: Array<EventType> }) {
     const income = events.filter(e => e.type === "income").reduce((acc, e) => acc + e.amount, 0);
     const expense = events.filter(e => e.type === "expense").reduce((acc, e) => acc + e.amount, 0);
+    const balance = income - expense;
 
     return (
         <div className="flex flex-col gap-1 text-sm text-gray-700 dark:text-gray-300">
             <div className="flex justify-between">
                 <span>Income:</span>
-                <span className="text-green-600">{currencyFormatter.format(income)}</span>
+                <span className="text-green-600 dark:text-green-400">{currencyFormatter.format(income)}</span>
             </div>
             <div className="flex justify-between">
                 <span>Expense:</span>
-                <span className="text-red-600">{currencyFormatter.format(expense)}</span>
+                <span className="text-red-400 dark:text-red-300">{currencyFormatter.format(expense)}</span>
             </div>
             <div className="flex justify-between font-semibold mt-1">
                 <span>Monthly:</span>
-                <span className="text-blue-600">{currencyFormatter.format(income - expense)}</span>
+                <span className="text-gray-700 dark:text-gray-200">{currencyFormatter.format(balance)}</span>
             </div>
             <div className="flex justify-between font-semibold mt-1">
                 <span>Global:</span>
-                <span className="text-blue-600">{currencyFormatter.format(income - expense)}</span>
+                <span className="text-gray-700 dark:text-gray-200">{currencyFormatter.format(balance)}</span>
             </div>
-        </div>
+        </div >
     );
 }
